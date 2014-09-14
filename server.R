ecb316572961ee1d52af5857ad6a159cb88baa87
CaryGrantAnderson <Grant@CaryGrantAnderson.com>
2014-09-14 13:42:28 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,23 +1,34 @@
 library(shiny)
 library(datasets)
-
+library("ggplot2")
 
 shinyServer(function(input, output) 
 {
+  attach(mtcars)
+  
+  # Variables Display.
   output$caption <- reactive({ paste("Current Plot: ", input$variable1, " versus ", input$variable2) })
   
   output$Var_1 <- reactive({ paste("Performance Variable: ", input$variable1) })
   output$Var_2 <- reactive({ paste("Factor Variable:      ", input$variable2) })
 
+  
+  # The Output Plot.
   environment <- environment() 
 
   output$mpgPlot <- renderPlot({
-    
-  attach(mtcars)
     
   thePlot <- ggplot(mtcars, aes (get(input$variable1), get(input$variable2)), environment = environment) + geom_point() + geom_smooth(method=input$smoother)
         
   thePlot + ggtitle(paste(input$variable1, " versus ", input$variable2)) + ylab(input$variable2) + xlab(input$variable1) + geom_line(stat="hline", yintercept="mean") + geom_line(stat="vline", xintercept="mean")
 
   })
+  
+  
+  # Factor Variables Calculations
+  
+  output$Min_Var <-  reactive({ paste("Min:  ", min(get(input$variable2))) })
+  output$Mean_Var <-  reactive({ paste("Min:  ", mean(get(input$variable2))) })
+  output$Max_Var <-  reactive({ paste("Min:  ", max(get(input$variable2))) })  
+
 })
